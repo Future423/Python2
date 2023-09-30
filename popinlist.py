4b48c981e3407466d220f15b315bff4f46ea3cd5
Nishant <72150854+Future423@users.noreply.github.com>
2023-09-30 18:35:08 +0530

    Create popinlist.py

---
+++ popinlist.py
@@ -0,0 +1,12 @@
+a=[1,2,3,4,5]
+print("the list is: ",a)
+b=a.pop()
+print("the list: ",a)
+print("the popped element is",b)
+
+#by giving index number
+c=[11,12,13,14,15]
+print("the list is: ",c)
+d=c.pop(2) #here 2 is index i.e 13
+pirnt("the list: ",c)
+print("the popped element is",d)
