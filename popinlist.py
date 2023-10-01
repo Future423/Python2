f71b25ecea4be96776b946ceab1228cd41a326dc
Sakshisheokand8 <134862782+Sakshisheokand8@users.noreply.github.com>
2023-10-01 11:04:57 +0530

    Update popinlist.py

--- popinlist.py
+++ popinlist.py
@@ -8,5 +8,5 @@ print("the popped element is",b)
 c=[11,12,13,14,15]
 print("the list is: ",c)
 d=c.pop(2) #here 2 is index i.e 13
-pirnt("the list: ",c)
+print("the list: ",c)
 print("the popped element is",d)
